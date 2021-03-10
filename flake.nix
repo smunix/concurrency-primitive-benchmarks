cef1b980a8596451138de774da4fda26552f6d35
Providence Salumu <Providence.Salumu@smunix.com>
2021-03-10 06:28:57 -0500

    flakes: initial commit

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A very basic flake";
+  inputs.fu.url = "github:numtide/flake-utils/master";
+  outputs = { self, nixpkgs, fu, ... }:
+    with fu.lib;
+    with nixpkgs.lib;
+    eachSystem [ "x86_64-linux" ] (system:
+      let version = "${substring 0 8 self.lastModifiedDate}.${self.shortRev or "dirty"}";
+          overlay = final: _:
+            with final;
+            with haskellPackages;
+            with haskell.lib;
+            {
+              apps = recurseIntoAttrs ({
+                concurrency-primitive-benchmarks =
+                  overrideCabal (callCabal2nix "concurrency-primitive-benchmarks" ./. {})
+                    (o: { version = o.version + "-" + version; });
+              });
+            };
+          overlays = [ overlay ];
+      in
+        with (import nixpkgs { inherit system overlays; });
+        rec {
+          packages = flattenTree (recurseIntoAttrs {
+            inherit apps;
+          });
+        }
+    );
+}
